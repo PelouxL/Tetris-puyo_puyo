1fa17e62ba5f3907bd0b4b5eadade60a98e0a6a8
PelouxL <152203841+PelouxL@users.noreply.github.com>
2024-11-04 16:36:31 +0100

    Add files via upload

---
+++ ini_poyo.h
@@ -0,0 +1,17 @@
+#ifndef _INI_POYO_H_
+#define _INI_POYO_H_
+#include "types.h"
+
+/*------------------ initialisation des poyos -------------*/
+void affiche_c_poyo(c_poyo * cp);
+
+void ini_poyo_chaine(c_poyo *tpoyo, int n);
+
+void initialisation_cpoyo(c_poyo * cp);
+
+void apparition_piece(c_poyo * p, grille *gr);
+
+void decalage_gauche(c_poyo *tpoyo, int n);
+/*------------------ fin d'initialisation ----------------*/
+
+#endif

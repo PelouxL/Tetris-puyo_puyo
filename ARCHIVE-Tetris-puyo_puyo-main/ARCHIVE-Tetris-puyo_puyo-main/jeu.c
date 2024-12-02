d5dbf3677fbc57a56346174f47d40e0dc3cf9524
Jean-dlf <Jean.delaforge22@gmail.com>
2024-12-03 00:00:42 +0100

    Add files via upload

---
+++ ARCHIVE-Tetris-puyo_puyo-main/ARCHIVE-Tetris-puyo_puyo-main/jeu.c
@@ -0,0 +1,104 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <MLV/MLV_all.h>
+#include <time.h>
+#include "types.h"
+#include "grille.h"
+#include "pieces.h"
+#include "ini_poyo.h"
+#include "affichage_mlv.h"
+#include "destruction.h"
+#include "menu.h"
+
+void jeu(grille *gr, joueur *je, c_poyo tpoyo[4]){
+    int temps, duree, pause = 0, pressed;
+    c_poyo ptmp;
+    MLV_Keyboard_button touche;
+    bouton t_bouton_pause[3];
+    
+    
+    struct timespec debut, fin, maintenant, dernier_avancement;
+
+    initialisation_cpoyo_vide(&ptmp);
+    aff_jeu();
+
+    clock_gettime( CLOCK_REALTIME, &dernier_avancement );
+  
+    while(1){
+
+        /* permet de de gerer le framrate */
+        clock_gettime( CLOCK_REALTIME, &debut );
+
+        /* permet de gérer le temps passer entre chaque avancement */
+        clock_gettime( CLOCK_REALTIME, &maintenant );
+
+        if(MLV_get_event(&touche, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL) == MLV_KEY){
+            if(touche == MLV_KEYBOARD_ESCAPE){
+                pause = 1;
+                 
+                while(pause == 1){
+                    MLV_clear_window(MLV_COLOR_BEIGE);
+                    menu_pause(t_bouton_pause);
+                    pressed = clic_bouton(t_bouton_pause, 3);
+                    switch(pressed){
+                    case 0:
+                        printf("Le jeu se relance \n");
+                        aff_jeu();
+                        aff_poyos(tpoyo, &ptmp);
+                        MLV_actualise_window();
+                        pause = 0;
+                        break;
+
+                    case 1:
+                        printf("Save \n");
+                        break;
+                    case 2:
+                        printf("Menu \n");
+                        break;
+                    }
+                }                  
+            }
+        }
+        if(pause == 0){
+            if( tpoyo[0].apparait == 0 ){
+                apparition_piece(&tpoyo[0], gr);
+            }
+            if( tpoyo[0].p1.pos == 1 && tpoyo[0].p2.pos == 1 ){
+                roulement_poyo(tpoyo, gr);
+            }
+            if(( maintenant.tv_sec - dernier_avancement.tv_sec ) > 1 ||  ( (maintenant.tv_sec - dernier_avancement.tv_sec) == 1 && maintenant.tv_nsec >= dernier_avancement.tv_nsec) ){
+                avancement_piece(&tpoyo[0], gr);
+                dernier_avancement = maintenant;
+            }
+
+            /* aff_grille(*gr); */
+            aff_etat(*gr, *je);
+            aff_poyos(tpoyo, &ptmp);
+        
+            printf("\n");
+
+
+            deplacement(&tpoyo[0], gr);
+            ptmp = sauvegarde_poyos(tpoyo, ptmp, gr);
+    
+            affiche_c_poyo(&tpoyo[0]);
+            actualisation_poyo(&tpoyo[0], gr);
+        
+            /* principe des coordonnées et de la destruction */
+            chute_et_destruction(&tpoyo[0], gr);
+
+            clock_gettime(CLOCK_REALTIME, &fin );
+    
+            /* Framerate */
+            duree = ((fin.tv_sec - debut.tv_sec) * 1000) + ((fin.tv_nsec - debut.tv_nsec) / 1000000);
+            temps = 16 - duree ;
+            if(temps > 0){
+                MLV_wait_milliseconds(temps);
+            }
+            if(temps > 50){
+                printf("%d\n", temps);
+            }
+            MLV_actualise_window();
+        }
+    }
+}
---
+++ ARCHIVE-Tetris-puyo_puyo-main/ARCHIVE-Tetris-puyo_puyo-main/pieces.c
@@ -0,0 +1,354 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "types.h"
+#include "ini_poyo.h"
+#include <MLV/MLV_all.h>
+
+
+/* est appeler une fois lors de la premire apparition d'un poyo */
+void apparition_piece(c_poyo * p, grille *gr){
+  p -> p1.y = gr -> m/2;
+  p -> p2.y = gr -> m/2;
+  p -> apparait = 1;
+}
+
+
+void actualisation_poyo(c_poyo *p, grille *gr){
+  if( p -> apparait == 1){
+    gr -> mat[p -> p1.x][p -> p1.y] = p -> p1.couleur;
+    if( p -> p2.y != -1 ){
+      gr -> mat[p -> p2.x][p -> p2.y] = p -> p2.couleur;
+    }
+  }
+  
+} /* pas sur de l'utilité de cette, peux surement être supprimer pour en modif une autre */
+
+int peux_bouger_droite(poyo p, grille gr){
+  if( gr.mat[p.x][p.y+1] == 0 && p.y < gr.m-1 ){
+    return 1;
+  }
+  return 0;
+}
+
+int peux_bouger_gauche(poyo p, grille gr){
+  if( gr.mat[p.x][p.y-1] == 0 && p.y > 0 ){
+    return 1;
+  }
+  return 0;
+}
+
+int peux_bouger_bas(poyo p, grille gr){
+  if( gr.mat[p.x+1][p.y] == 0 && p.x < gr.n-1 ){
+    return 1;
+  }
+  return 0;
+}
+
+
+int disposition(c_poyo p){
+  if( p.p1.x >= p.p2.x ){
+    if( p.p1.y < p.p2.y ){ /* p2 a gauche de p1 */
+      return 1;
+    }
+    if( p.p1.y == p.p2.y ){ /* p2 au dessus de p1 */
+      return 2;
+    }
+    else{
+      return 3; /* p2 a droite de p1 */
+    }
+  }
+  return 4; /* p2 en dessous de p1 */
+}
+
+
+void avancement_piece(c_poyo * p, grille *gr){
+  int d = -1;
+  
+  if( p -> p1.pos == 0 && p -> p2.pos == 0){ /* si poyo est seul alors pas connaitre disposition */
+    d = disposition(*p);
+  }
+  if( ( peux_bouger_bas(p -> p1, *gr) == 1  || ( peux_bouger_bas(p -> p1, *gr) == 0  && peux_bouger_bas(p -> p2, *gr) == 1 && d == 4 ) ) &&  p -> p1.pos == 0 ){
+    gr -> mat[p -> p1.x][p -> p1.y] = 0;
+    p -> p1.x += 1;
+  }else{
+    if( d%2 == 0){
+      p -> p2.pos = 1;
+    }
+    p -> p1.pos = 1;
+  }
+
+  if( peux_bouger_bas(p -> p2, *gr) == 1 && p -> p2.pos == 0 ){
+    gr -> mat[p -> p2.x][p -> p2.y] = 0;
+    p -> p2.x += 1;
+  }else{
+    if( d%2 == 0 ){
+      p -> p1.pos = 1;
+    }
+    p -> p2.pos = 1;
+  }
+}
+
+
+/* --------------------------- definition des fonction de deplacement ------------------------- */
+
+void pivot_droit(c_poyo *p, grille *gr){
+  int d;
+  d = disposition(*p);
+  if( MLV_get_keyboard_state(MLV_KEYBOARD_d) == MLV_PRESSED ){
+    if( p -> p1.pos == 0 && p -> p2.pos == 0 ){
+      switch ( d )
+	{
+	case 1:
+	  if( peux_bouger_bas( p -> p1, *gr) == 1 ){
+	    gr -> mat[p -> p2.x][p -> p2.y] = 0;
+	    p -> p2.x = p -> p1.x + 1;
+	    p -> p2.y = p -> p1.y;
+	  }
+	  break;
+	case 2:
+	  if( peux_bouger_droite( p -> p1, *gr) == 1 ){
+	    gr -> mat[p -> p2.x][p -> p2.y] = 0;
+	    p -> p2.x = p -> p1.x;
+	    p -> p2.y = p -> p1.y + 1;
+	  }
+	  break;
+	case 3:
+	  gr -> mat[p -> p2.x][p -> p2.y] = 0;
+	  p -> p2.x = p -> p1.x - 1;
+	  p -> p2.y = p -> p1.y;
+	  break;
+	case 4:
+	  if( peux_bouger_gauche( p -> p1, *gr ) == 1 ){
+	    gr -> mat[p -> p2.x][p -> p2.y] = 0;
+	    p -> p2.x = p -> p1.x;
+	    p -> p2.y = p -> p1.y - 1;
+	  }
+	  break;
+	}
+    }
+  }
+}
+
+void pivot_gauche(c_poyo *p, grille *gr){
+  int d;
+  d = disposition(*p);
+  if( MLV_get_keyboard_state(MLV_KEYBOARD_q) == MLV_PRESSED ){
+    if( p -> p1.pos == 0 && p -> p2.pos == 0 ){
+      switch ( d )
+	{
+	case 1:
+	  gr -> mat[p -> p2.x][p -> p2.y] = 0;
+	  p -> p2.x = p -> p1.x - 1;
+	  p -> p2.y = p -> p1.y;
+	  break;
+	case 2:
+	  if( peux_bouger_gauche( p -> p1, *gr) == 1){
+	    gr -> mat[p -> p2.x][p -> p2.y] = 0;
+	    p -> p2.x = p -> p1.x;
+	    p -> p2.y = p -> p1.y - 1;
+	  }
+	  break;
+	case 3:
+	  if( peux_bouger_bas( p -> p1, *gr) == 1){
+	    gr -> mat[p -> p2.x][p -> p2.y] = 0;
+	    p -> p2.x = p -> p1.x + 1;
+	    p -> p2.y = p -> p1.y;
+	  }
+	  break;
+	case 4:
+	  if( peux_bouger_droite( p -> p1, *gr) == 1){
+	    gr -> mat[p -> p2.x][p -> p2.y] = 0;
+	    p -> p2.x = p -> p1.x;
+	    p -> p2.y = p -> p1.y + 1;
+	  }
+	  break;
+	}
+    }
+  }
+}
+
+void deplacement_droit( c_poyo *p, grille *gr){
+  int d;
+  poyo *ptmp = &p -> p1;
+
+  if( MLV_get_keyboard_state(MLV_KEYBOARD_RIGHT) == MLV_PRESSED ){
+    if ( p -> p1.pos == 0 && p -> p2.pos == 0 ){
+      d = disposition(*p);
+      switch ( d%2 )
+	{
+	case 0 :
+	  if( peux_bouger_droite( p->p2, *gr) == 1 &&  peux_bouger_droite( p->p1, *gr) == 1 ){
+	    gr -> mat[p -> p1.x][p -> p1.y] = 0;
+	    gr -> mat[p -> p2.x][p -> p2.y] = 0;
+	    p -> p2.y += 1;
+	    p -> p1.y += 1;
+	  }
+	  break;
+	case 1 :
+	  if( d == 1){
+	    ptmp = &p -> p2;
+	  }
+	  if( peux_bouger_droite( *ptmp, *gr) == 1 ){
+	    gr -> mat[p -> p1.x][p -> p1.y] = 0;
+	    gr -> mat[p -> p2.x][p -> p2.y] = 0;
+	    p -> p1.y += 1;
+	    p -> p2.y += 1;
+	  
+	  }
+	  break;
+	}
+    }else{
+      if( p -> p1.pos == 1 && p -> p2.pos == 0 ){
+	ptmp = &p -> p2;
+      }
+      if( peux_bouger_droite( *ptmp, *gr) == 1 ){
+	gr -> mat[ptmp -> x][ptmp -> y] = 0;
+	ptmp -> y += 1;
+      }
+    }
+  }
+}	
+
+
+
+void deplacement_gauche( c_poyo *p, grille *gr){
+  int d;
+  poyo *ptmp = &p -> p1;
+
+  if( MLV_get_keyboard_state(MLV_KEYBOARD_LEFT) == MLV_PRESSED ){
+    if ( p -> p1.pos == 0 && p -> p2.pos == 0 ){
+      d = disposition(*p);
+      switch ( d%2 )
+	{
+	case 0 :
+            if( (peux_bouger_gauche( p->p2, *gr) == 1 &&  peux_bouger_gauche( p->p1, *gr) == 1 ) || (peux_bouger_gauche( p->p1, *gr) == 1 && p -> p2.x == -1) ){
+	    gr -> mat[p -> p1.x][p -> p1.y] = 0;
+	    gr -> mat[p -> p2.x][p -> p2.y] = 0;
+	    p -> p2.y -= 1;
+	    p -> p1.y -= 1;
+	  }
+	  break;
+	case 1 :
+	  if( d == 3){
+	    ptmp = &p -> p2;
+	  }
+	  if( peux_bouger_gauche( *ptmp, *gr) == 1 ){
+	    gr -> mat[p -> p1.x][p -> p1.y] = 0;
+	    gr -> mat[p -> p2.x][p -> p2.y] = 0;
+	    p -> p1.y -= 1;
+	    p -> p2.y -= 1;
+	  
+	  }
+	  break;
+	}
+    }else{
+      if( p -> p1.pos == 1 && p -> p2.pos == 0 ){
+	ptmp = &p -> p2;
+      }
+      if( peux_bouger_gauche( *ptmp, *gr ) == 1 ){
+	gr -> mat[ptmp -> x][ptmp -> y] = 0;
+	ptmp -> y -= 1;
+      }
+    }
+  }
+}	
+
+
+void deplacement_bas( c_poyo *p, grille *gr){
+  int d;
+  poyo *ptmp = &p -> p1;
+
+  if( MLV_get_keyboard_state(MLV_KEYBOARD_DOWN) == MLV_PRESSED ){
+    if ( p -> p1.pos == 0 && p -> p2.pos == 0 ){
+      d = disposition(*p);
+      switch ( d%2 )
+	{
+	case 0 :
+	  if( d == 4 ){
+	    ptmp = &p -> p2;
+	  }
+	  if( peux_bouger_bas( *ptmp, *gr) == 1 ){
+	    gr -> mat[p -> p1.x][p -> p1.y] = 0;
+	    gr -> mat[p -> p2.x][p -> p2.y] = 0;
+	    p -> p2.x += 1;
+	    p -> p1.x += 1;
+	  }
+	  break;
+	case 1 :
+	  if( peux_bouger_bas( p -> p1, *gr) == 1 &&  peux_bouger_bas( p -> p2, *gr) == 1 ){
+	    gr -> mat[p -> p1.x][p -> p1.y] = 0;
+	    gr -> mat[p -> p2.x][p -> p2.y] = 0;
+	    p -> p1.x += 1;
+	    p -> p2.x += 1;
+	  
+	  }
+	  break;
+	}
+    }else{
+      if( p -> p1.pos == 1 && p -> p2.pos == 0 ){
+	ptmp = &p -> p2;
+      }
+      if( peux_bouger_bas( *ptmp, *gr) == 1 ){
+	gr -> mat[ptmp -> x][ptmp -> y] = 0;
+	ptmp -> x += 1;
+      }
+    }
+  }
+}
+
+void deplacement(c_poyo *p, grille *gr){
+  static int tmp_passer = 0;
+  int tmp_act, attente;
+  tmp_act = MLV_get_time(), attente = 150;
+  if( MLV_get_keyboard_state(MLV_KEYBOARD_DOWN) == MLV_PRESSED || MLV_get_keyboard_state(MLV_KEYBOARD_LEFT) == MLV_PRESSED || MLV_get_keyboard_state(MLV_KEYBOARD_RIGHT) == MLV_PRESSED || MLV_get_keyboard_state(MLV_KEYBOARD_q) == MLV_PRESSED || MLV_get_keyboard_state(MLV_KEYBOARD_d) == MLV_PRESSED ){
+      
+    if( tmp_act - tmp_passer >= attente ){
+        
+      pivot_gauche(p, gr);
+      pivot_droit(p, gr);
+      deplacement_droit(p, gr);
+      deplacement_gauche(p, gr);
+      deplacement_bas(p, gr);
+
+      tmp_passer = tmp_act;
+    }
+  }
+}
+
+
+
+/* -------------------------------- fin des definition de deplacement -------------------------------------*/
+
+c_poyo sauvegarde_poyos(c_poyo *tpoyo, c_poyo poyo_sauvegarde, grille *gr){
+    c_poyo ptmp;
+    static int tmp_passer = 0;
+    int tmp_act, attente;
+    tmp_act = MLV_get_time(), attente = 500;
+    
+    if( MLV_get_keyboard_state(MLV_KEYBOARD_SPACE) == MLV_PRESSED){
+
+        if( tmp_act - tmp_passer >= attente ){
+             
+            if( poyo_sauvegarde.p1.couleur == 0){
+                poyo_sauvegarde = tpoyo[0];
+                gr -> mat[ tpoyo[0].p1.x ][tpoyo[0].p1.y ] = 0;
+                gr -> mat[ tpoyo[0].p2.x ][tpoyo[0].p2.y ] = 0;
+                roulement_poyo(tpoyo, gr);
+            }else{
+                ptmp = tpoyo[0];
+                gr -> mat[ tpoyo[0].p1.x ][tpoyo[0].p1.y ] = 0;
+                gr -> mat[ tpoyo[0].p2.x ][tpoyo[0].p2.y ] = 0;
+                tpoyo[0] = poyo_sauvegarde;
+                poyo_sauvegarde = ptmp;
+                
+            }
+            tmp_passer = tmp_act;
+        }
+    }
+    poyo_sauvegarde.apparait = 0;
+    poyo_sauvegarde.p1.x = 0;
+    poyo_sauvegarde.p1.y = 0;
+    poyo_sauvegarde.p2.x = -1;
+    poyo_sauvegarde.p2.y = -1;
+    return poyo_sauvegarde;
+}
